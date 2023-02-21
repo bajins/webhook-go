b2c380ff8f8c63c5cffb9ef6921e1e0c75578e41
woytu <394585829@qq.com>
2023-02-21 19:42:43 +0800

    update

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+	golang.org/x/crypto v0.6.0
 )
--- go_pack_embed.sh
+++ go_pack_embed.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 需要打包的静态文件或目录
-static_files=(data)
-
 # 脚本当前目录绝对路径
 project_path=$(
   cd "$(dirname "$0")" || exit
@@ -12,35 +9,27 @@ project_path=$(
 project_name="${project_path##*/}"
 
 # 获得所有受支持平台的列表
+#os_arch_array=($(go tool dist list))
+#for i in "${!os_arch_array[@]}"; do
+#  item=${array[i]}
 os_arch_array=$(go tool dist list)
 
 # 保存默认分隔符
 OLD_IFS="$IFS"
 for item in ${os_arch_array}; do
-  # array=("${item//\// }")# 替换\为空格（IFS的默认分隔符）
+  # array=(${item//\// }) # 替换\为空格（IFS的默认分隔符）
   # 指定分隔符
   IFS="/"
   array=(${item})
 
   os=${array[0]}
   arch=${array[1]}
-  zip_dir=${project_name}_${os}_${arch}
+  binary_file=${project_name}_${os}_${arch}
 
   # 忽略以下平台的编译
   if [[ "$os" == "android" ]] || [[ "$os" == "ios" ]] || [[ "$os" == "darwin" && "$arch" == *arm* ]]; then
     continue
   fi
-
-  if [ -e "$zip_dir" ]; then
-    # 如果压缩目录存在则删除
-    rm -rf "$zip_dir"
-  fi
-
-  # 创建要打包的目录，把静态文件复制进去
-  mkdir "$zip_dir"
-  for static_file in "${static_files[@]}"; do
-    cp -r "$static_file" "$zip_dir"
-  done
 
   # 设置变量
   export GOOS="$os" GOARCH="$arch"
@@ -48,7 +37,6 @@ for item in ${os_arch_array}; do
   echo "环境变量设置成功：$GOOS------$GOARCH"
 
   # 指定编译的二进制文件名
-  binary_file="$zip_dir"
   if [ "$os" == "windows" ]; then
     binary_file="$binary_file".exe
   fi
@@ -73,19 +61,8 @@ for item in ${os_arch_array}; do
     export CGO_ENABLED=0
     flags="-w"
   fi
-  # 编译二进制文件并输出到指定目录
-  go build -ldflags=$flags -o "$zip_dir/$binary_file"
-
-  # 压缩
-  if [ "$os" == "windows" ]; then
-    zip -q -r "$zip_dir".zip "$zip_dir"
-  else
-    tar -czf "$zip_dir".tar.gz "$zip_dir"
-  fi
-
-  # 删除压缩目录
-  rm -rf "$zip_dir"
-
+  # 编译二进制文件并输出到当前目录
+  go build -ldflags=$flags -o "build/$binary_file"
 done
 
 # 还原默认分隔符
